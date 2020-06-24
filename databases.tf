5118fa8293306566a75282cab91aec3b44c963fd
Eliezio Oliveira <eliezio@pm.me>
2020-06-24 18:03:44 +0100

    Change DB ingress rule to become a std "All TCP"

--- databases.tf
+++ databases.tf
@@ -77,8 +77,8 @@ resource aws_security_group rds {
   ingress {
     description = "sg inbound to itself - glue requirement"
     from_port   = 0
-    to_port     = 0
-    protocol    = -1
+    to_port     = 65535
+    protocol    = "tcp"
     self        = true
   }
 
